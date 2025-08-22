950a9010826af7d39d56b7813596fe94fbb7e1cb
SchweGELBin <abramjannikmichael06@gmail.com>
2025-08-22 19:38:37 +0200

    feat: create basic layout

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,52 @@
-use crossterm::event::{self, Event};
-use ratatui::{text::Text, Frame};
+use color_eyre::Result;
+use ratatui::{
+    crossterm::event::{self, Event, KeyCode, KeyEventKind},
+    layout::{Constraint, Direction, Layout, Rect},
+    widgets::{Block, BorderType, Borders, Paragraph},
+    DefaultTerminal, Frame,
+};
 
-fn main() {
-    let mut terminal = ratatui::init();
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
     loop {
-        terminal.draw(draw).expect("failed to draw frame");
-        if matches!(event::read().expect("failed to read event"), Event::Key(_)) {
-            break;
+        terminal.draw(draw)?;
+        if let Event::Key(key) = event::read()? {
+            if key.kind == KeyEventKind::Press && key.code == KeyCode::Char('q') {
+                break Ok(());
+            }
         }
     }
-    ratatui::restore();
 }
 
 fn draw(frame: &mut Frame) {
-    let text = Text::raw("Hello World!");
-    frame.render_widget(text, frame.area());
+    let layout = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints(vec![
+            Constraint::Percentage(8),
+            Constraint::Percentage(8),
+            Constraint::Percentage(8),
+            Constraint::Percentage(54),
+            Constraint::Percentage(24),
+        ])
+        .split(frame.area());
+    render_block(frame, layout[0], "Translation", &Paragraph::new("Test"));
+    render_block(frame, layout[1], "Book", &Paragraph::new("Test"));
+    render_block(frame, layout[2], "Chapter", &Paragraph::new("Test"));
+    render_block(frame, layout[3], "Read", &Paragraph::new("Test"));
+    render_block(frame, layout[4], "Search", &Paragraph::new("Test"));
+}
+
+fn render_block(frame: &mut Frame, area: Rect, title: &str, paragraph: &Paragraph) {
+    let block = Block::new()
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded)
+        .title(title);
+    frame.render_widget(paragraph.clone().block(block), area);
 }
